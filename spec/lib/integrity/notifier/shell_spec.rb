cd86af38f172e6a77b2748c3047a5306ab8a3718
Michael Glass <michael.glass@gmail.com>
2009-05-28 14:43:44 -0400

    sharing tests, not yet functional

--- spec/lib/integrity/notifier/shell_spec.rb
+++ spec/lib/integrity/notifier/shell_spec.rb
@@ -1,4 +1,57 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe Shell do
-  describe "#self_to_haml"
+  describe "#deliver!" do
+  
+    context "a passing commit is pushed" do
+      before(:each) do
+        @passing = Integrity::Commit.gen(:successful)
+      end
+      context "if the passing script exist" do
+        before(:each) do
+          @config = {'passing_script' => File.expand_path(File.dirname(__FILE__) + '/test_pass'), 'failing_script' => ''}          
+        end
+        it "should run the passing script" do
+        
+        end
+        end
+      end
+    
+      context "if the passing script doesn't exist" do
+        context "if the passing script is blank" do
+          before(:each) do 
+            @config = {'passing_script' => '', 'failing_script' => ''}
+          end
+          it "should not try to run the script"
+        end
+  
+        context "if the passing script is not blank" do
+          before(:each) do 
+            @config = {'passing_script' =>  File.expand_path(File.dirname(__FILE__) +'/test_pass_fake', 'failing_script' => ''}
+          end
+     
+          it "should log an error"
+        end
+      end
+    end
+    
+    context "a failing commit is pushed" do
+      before(:each) do
+        @failing = Integrity::commit.gen(:failed)
+      end
+      context "if the failing script exists" do
+        before(:each) do
+          @config = {'failing_script' => File.expand_path(File.dirname(__FILE__) + '/test_fail'), 'passing_script' => ''}
+        end
+        it "should run the failing script"
+      end
+        
+      context "if the failing script does not exist" do
+        before(:each) do 
+          @config = {'passing_script' => '', 'failing_script' => ''}
+        end
+        it "should not try tp run the failing script"
+      end
+    end
+  end
+end

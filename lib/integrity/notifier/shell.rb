109fd87ca73b4d008ce4a0849bc5f93787185f15
Michael Glass <michael.glass@gmail.com>
2009-05-28 14:03:55 -0400

    just sycning up.  this is broken

--- lib/integrity/notifier/shell.rb
+++ lib/integrity/notifier/shell.rb
@@ -2,6 +2,15 @@ require 'integrity'
 module Integrity
   class Notifier
     class Shell < Notifier::Base
+      def self.to_haml
+        def self.to_haml
+          File.read(File.dirname(__FILE__) + "/config.haml")
+        end
+        def deliver!
+          raise NotImplementedError, "you need to implement this method in your notifier"
+        end
+        
+      end
     end
   end
 end
---
+++ spec/lib/integrity/notifier/shell_spec.rb
@@ -0,0 +1,4 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
+
+describe Shell do
+  describe "#self_to_haml"
